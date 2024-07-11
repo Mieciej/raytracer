550724227d993b927db711c8f164fe4e3b6fbf4e
Maciej Janicki <maciej.ja@proton.me>
2024-07-11 13:17:37 +0200

    Feature: reflections!

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ typedef struct {
   Vector3 origin;
   Color color;
   int specular;
+  float reflective;
 } sphere_t;
 
 const float ambient_light = 0.2;
@@ -22,10 +23,11 @@ const int cw = 1000;
 const int ch = 1000;
 
 Vector3 canvas_to_viewport(int x, int y);
-Color trace_ray(Vector3 O, Vector3 D, float t_min, float t_max);
+Color trace_ray(Vector3 O, Vector3 D, float t_min, float t_max, size_t rec);
 bool trace_shadow_ray(Vector3 P, Vector3 L, float t_min, float t_max);
 void intersect_sphere(Vector3, Vector3, const sphere_t *, float *, float *);
 float compute_lighting(Vector3 P, Vector3 N, Vector3 V, sphere_t *s);
+Vector3 reflect_ray(Vector3 R, Vector3 N);
 
 sphere_t spheres[4];
 light_t lights[2];
@@ -38,30 +40,34 @@ int main(void) {
   spheres[0] = (sphere_t){.r = 1,
                           .origin = {.x = 0, .y = -1, .z = 3},
                           .color = RED,
-                          .specular = 100};
+                          .specular = 500,
+                          .reflective = 0.2};
   spheres[1] = (sphere_t){.r = 1,
                           .origin = {.x = 2, .y = 0, .z = 4},
                           .color = BLUE,
-                          .specular = 100};
+                          .specular = 500,
+                          .reflective = 0.3};
   spheres[2] = (sphere_t){.r = 1,
                           .origin = {.x = -2, .y = 0, .z = 4},
                           .color = GREEN,
-                          .specular = 500};
+                          .specular = 10,
+                          .reflective = 0.4};
   spheres[3] = (sphere_t){.r = 5000,
                           .origin = {.x = 0, .y = -5001, .z = 0},
                           .color = YELLOW,
-                          .specular = -1};
+                          .specular = 1000,
+                          .reflective = 0.5};
   lights[0] = (light_t){.type = POINT, .intensity = 0.6, .position = {2, 1, 0}};
   lights[1] =
       (light_t){.type = DIRECTIONAL, .intensity = 0.2, .position = {1, 4, 4}};
 
   while (!WindowShouldClose()) {
     BeginDrawing();
-    ClearBackground(RAYWHITE);
+    ClearBackground(BLACK);
     for (int x = -cw / 2; x < cw / 2; x++) {
       for (int y = -ch / 2; y < ch / 2; y++) {
         Vector3 D = canvas_to_viewport(x, y);
-        Color color = trace_ray(O, D, 1, INFINITY);
+        Color color = trace_ray(O, D, 1, INFINITY, 3);
         DrawPixel(x + cw / 2, -y + ch / 2, color);
       }
     }
@@ -76,7 +82,7 @@ Vector3 canvas_to_viewport(int x, int y) {
   return (Vector3){.x = x * (1.0 / cw), .y = y * (1.0 / ch), .z = 1};
 }
 
-Color trace_ray(Vector3 O, Vector3 D, float t_min, float t_max) {
+Color trace_ray(Vector3 O, Vector3 D, float t_min, float t_max, size_t rec) {
   float closest_t = INFINITY;
   sphere_t closest_sphere = {0};
   for (size_t i = 0; i < 4; i++) {
@@ -92,18 +98,29 @@ Color trace_ray(Vector3 O, Vector3 D, float t_min, float t_max) {
     }
   }
   if (closest_sphere.r == 0) {
-    return RAYWHITE;
+    return BLACK;
   }
   Vector3 P = Vector3Add(O, Vector3Scale(D, closest_t));
   Vector3 ps = Vector3Subtract(P, closest_sphere.origin);
   Vector3 N = Vector3Normalize(ps);
   const Vector3 V = Vector3Scale(D, -1.0f);
   float i = compute_lighting(P, N, V, &closest_sphere);
-  Color ret = closest_sphere.color;
-  ret.r = Clamp(ret.r * i, 0, 255);
-  ret.g = Clamp(ret.g * i, 0, 255);
-  ret.b = Clamp(ret.b * i, 0, 255);
-  return ret;
+  Color local_color = closest_sphere.color;
+  local_color.r = Clamp(local_color.r * i, 0, 255);
+  local_color.g = Clamp(local_color.g * i, 0, 255);
+  local_color.b = Clamp(local_color.b * i, 0, 255);
+  if (rec <= 0 || closest_sphere.reflective <= 0) {
+    return local_color;
+  }
+  Vector3 R = reflect_ray(V, N);
+  Color reflected_color = trace_ray(P, R, 0.1, INFINITY, rec - 1);
+  reflected_color.r = reflected_color.r * closest_sphere.reflective +
+                      local_color.r * (1 - closest_sphere.reflective);
+  reflected_color.g = reflected_color.g * closest_sphere.reflective +
+                      local_color.g * (1 - closest_sphere.reflective);
+  reflected_color.b = reflected_color.b * closest_sphere.reflective +
+                      local_color.b * (1 - closest_sphere.reflective);
+  return reflected_color;
 }
 bool trace_shadow_ray(Vector3 P, Vector3 L, float t_min, float t_max) {
 
@@ -153,9 +170,7 @@ float compute_lighting(Vector3 P, Vector3 N, Vector3 V, sphere_t *s) {
       i += light->intensity * n_dot_l / (Vector3Length(N) * Vector3Length(L));
     }
     if (s->specular != -1) {
-      float n_dot_l2 = Vector3DotProduct(N, L);
-      Vector3 R = Vector3Scale(N, n_dot_l2 * 2);
-      R = Vector3Subtract(R, L);
+      Vector3 R = reflect_ray(L, N);
       float r_dot_v = Vector3DotProduct(R, V);
       if (r_dot_v > 0) {
         i += light->intensity *
@@ -164,4 +179,10 @@ float compute_lighting(Vector3 P, Vector3 N, Vector3 V, sphere_t *s) {
     }
   }
   return i;
+}
+Vector3 reflect_ray(Vector3 R, Vector3 N) {
+  float n_dot_r = Vector3DotProduct(R, N);
+  Vector3 ret = Vector3Scale(N, 2 * n_dot_r);
+  ret = Vector3Subtract(ret, R);
+  return ret;
 }
